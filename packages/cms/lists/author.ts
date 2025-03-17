48e119f4b585260528e94abad5d3a3d56e396cf3
nickhsine <nickhsine97753017@gmail.com>
2025-03-17 16:15:55 +0800

    feat(cms): index authors on Algolia

--- packages/cms/lists/author.ts
+++ packages/cms/lists/author.ts
@@ -1,3 +1,4 @@
+import envVars from '../environment-variables'
 import { list } from '@keystone-6/core'
 import { relationship, text, timestamp } from '@keystone-6/core/fields'
 import {
@@ -6,6 +7,9 @@ import {
   RoleEnum,
 } from './utils/access-control-list'
 import { slugConfig } from './config'
+import { algoliasearch } from 'algoliasearch'
+import { authorIndexName, getAuthorObjectID } from './utils/algolia'
+import errors from '@twreporter/errors'
 
 const listConfigurations = list({
   fields: {
@@ -58,5 +62,86 @@ const listConfigurations = list({
   ui: {
     label: 'Authors',
   },
+  hooks: {
+    afterOperation: async ({ operation, item, originalItem, context }) => {
+      if (!envVars.enableAlgoliaFeatureToggle) {
+        return
+      }
+
+      try {
+        const indexName = authorIndexName
+        const client = algoliasearch(
+          envVars.algolia.appID,
+          envVars.algolia.apiKey
+        )
+
+        const objectID = getAuthorObjectID((item || originalItem).id.toString())
+
+        if (operation === 'delete') {
+          try {
+            await client.deleteObject({
+              indexName,
+              objectID,
+            })
+
+            return
+          } catch (_error) {
+            const error = errors.helpers.wrap(
+              _error,
+              'Author.hooks.afterOperation error',
+              'Error to delete Algolia object.',
+              { objectID, indexName }
+            )
+            throw error
+          }
+        }
+
+        const { avatar, image: ogImage } = await context.query.Author.findOne({
+          where: {
+            id: item?.id.toString(),
+          },
+          query: 'avatar { resized { medium } }, image { resized { medium } } ',
+        })
+
+        const record = {
+          url: `${envVars.kidsWebsiteUrlOrigin}/author/${item?.slug}`,
+          name: item?.name,
+          email: item?.email,
+          bio: item?.bio,
+          avatarSrc: avatar?.resized?.medium,
+          ogImgSrc: ogImage?.resized?.medium,
+        }
+
+        try {
+          await client.partialUpdateObject({
+            indexName,
+            objectID,
+            attributesToUpdate: record,
+            createIfNotExists: true,
+          })
+        } catch (_error) {
+          const error = errors.helpers.wrap(
+            _error,
+            'Author.hooks.afterOperation error',
+            'Error to partial update an object in Algolia.',
+            { indexName, objectID, AttributeToUpdate: record }
+          )
+          throw error
+        }
+      } catch (error) {
+        console.log(
+          JSON.stringify({
+            severity: 'ERROR',
+            message: errors.helpers.printAll(error, {
+              withStack: true,
+              withPayload: true,
+            }),
+          })
+        )
+        throw error
+      }
+    },
+  },
 })
+
 export default listConfigurations
